24fe0c10490f5cc65884869d0098cb5347c341f6
kaiteen1003 <piasonica1003@gmail.com>
2025-07-05 14:14:35 +0900

    ヘッダーアイコンの作成と挿入

--- frontend/src/components/GlobalHeader.tsx
+++ frontend/src/components/GlobalHeader.tsx
@@ -1,5 +1,5 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-import PeopleIcon from "@mui/icons-material/People";
+import Image from "next/image";
 import Link from "next/link";
 
 export interface GlobalHeaderProps {
@@ -12,14 +12,22 @@ export function GlobalHeader({ title, subtitle }: GlobalHeaderProps) {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar
-          variant="dense"
           sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
             background:
               "linear-gradient(45deg, rgb(0, 91, 172), rgb(94, 194, 198))",
           }}
         >
           <Link href="/">
-            <PeopleIcon fontSize={"large"} sx={{ mr: 2 }} />
+            <Image
+              src="/icon.png"
+              alt="トップページ"
+              width={50}
+              height={40}
+              style={{ marginLeft: 0 }}
+            />
           </Link>
           <Link href="/">
             <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
