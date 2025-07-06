67d98f9863d955ee944c34bd8ed82bb15e353631
kaiteen1003 <piasonica1003@gmail.com>
2025-07-06 10:47:03 +0900

    frontendでの社員情報の型定義の作成

--- frontend/src/models/Employee.ts
+++ frontend/src/models/Employee.ts
@@ -1,9 +1,27 @@
 import * as t from "io-ts";
 
-export const EmployeeT = t.type({
-  id: t.string,
+// スキル型（io-ts）
+export const SkillT = t.type({
   name: t.string,
-  age: t.number,
+  level: t.string,
 });
 
-export type Employee = t.TypeOf<typeof EmployeeT>;
+export const EmployeeT = t.intersection([
+  t.partial({
+    skills: t.array(SkillT),
+    certifications: t.array(t.string),
+  }),
+  t.type({
+    id: t.string,
+    name: t.string,
+    age: t.number,
+  }),
+]);
+
+export type Employee = {
+  id: string;
+  name: string;
+  age: number;
+  skills?: { name: string; level: string }[];
+  certifications?: string[];
+};
