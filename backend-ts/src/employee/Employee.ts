011dd1266698620e7a2752624daab3af8df9c98b
kaiteen1003 <piasonica1003@gmail.com>
2025-07-06 09:16:12 +0900

    型定義を追加

--- backend-ts/src/employee/Employee.ts
+++ backend-ts/src/employee/Employee.ts
@@ -1,9 +1,23 @@
-import * as t from 'io-ts';
+import * as t from "io-ts";
 
-export const EmployeeT = t.type({
-    id: t.string,
-    name: t.string,
-    age: t.number,
+// スキル型（io-ts形式）
+const SkillT = t.type({
+  name: t.string,
+  level: t.union([
+    t.literal("Beginner"),
+    t.literal("Intermediate"),
+    t.literal("Advanced"),
+  ]),
 });
 
+// Employee 型（io-ts形式）
+export const EmployeeT = t.type({
+  id: t.string,
+  name: t.string,
+  age: t.number,
+  skills: t.union([t.array(SkillT), t.undefined]),
+  certifications: t.union([t.array(t.string), t.undefined]),
+});
+
+export type Skill = t.TypeOf<typeof SkillT>;
 export type Employee = t.TypeOf<typeof EmployeeT>;
